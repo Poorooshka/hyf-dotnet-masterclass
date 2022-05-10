90b685e1d8becc561c4fa7b30de5917e029fb9e6
Nashmil <nse@leapwork.com>
2022-05-10 18:36:43 +0200

    read connection string from the environment variable

--- final-project/MealsharingNET/Services/Shared.cs
+++ final-project/MealsharingNET/Services/Shared.cs
@@ -1,4 +1,7 @@
+
 public class Shared
 {
-    public const string ConnectionString = "Server=localhost;Database=meal_sharing;Uid=root;Pwd=????????;";
+    public static string ConnectionString = string.IsNullOrEmpty(Environment.GetEnvironmentVariable("MYSQLCONNSTR_MealSharingDb")) ?
+   "Server=localhost;Database=final-semih;Uid=root;Pwd=compl3xPassWrd;Convert Zero Datetime=True" :
+   Environment.GetEnvironmentVariable("MYSQLCONNSTR_MealSharingDb");
 }
