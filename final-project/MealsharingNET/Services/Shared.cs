1e530a56305204c0d4d4a7864453e7eb95b1bd0b
Nashmil <nse@leapwork.com>
2022-05-10 19:34:10 +0200

    enviromental variable for local edited

--- final-project/MealsharingNET/Services/Shared.cs
+++ final-project/MealsharingNET/Services/Shared.cs
@@ -2,6 +2,6 @@
 public class Shared
 {
     public static string ConnectionString = string.IsNullOrEmpty(Environment.GetEnvironmentVariable("MYSQLCONNSTR_MealSharingDb")) ?
-   "Server=localhost;Database=final-semih;Uid=root;Pwd=compl3xPassWrd;Convert Zero Datetime=True" :
+   "Server=localhost;Database=meal_sharing;Uid=root;Pwd=????????;Convert Zero Datetime=True" :
    Environment.GetEnvironmentVariable("MYSQLCONNSTR_MealSharingDb");
 }
